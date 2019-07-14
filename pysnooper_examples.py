93a6dda63177df16cf0ddfb06bee155cac506b41
vuduclyunitn <ducly.vu@unitn.it>
2019-07-14 21:19:58 +0200

    Py snooper

---
+++ pysnooper_examples.py
@@ -0,0 +1,15 @@
+import pysnooper
+
+#@pysnooper.snoop()
+def number_to_bits(number):
+    if number:
+        bits = []
+        while number:
+            number, remainder = divmod(number, 2)
+            with pysnooper.snoop():
+                bits.insert(0, remainder)
+        return bits
+    else:
+        return [0]
+
+number_to_bits(6)
