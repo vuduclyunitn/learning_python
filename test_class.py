a948b8921380be1385f71c3f7617883f45f4cc10
vuduclyunitn <32923740+vuduclyunitn@users.noreply.github.com>
2019-07-29 01:33:36 -0700

    Create test_class.py

---
+++ test_class.py
@@ -0,0 +1,8 @@
+# Nội dụng của test_class.py
+class TestClass(object):
+  def test_one(self):
+    x = "this"
+    assert "h" in x
+  def test_two(self):
+    x = "hello"
+    assert hasattr(x, "check")
